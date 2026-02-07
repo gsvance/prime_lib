96adadf726effa5143b787596517f23c09d0cb26
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-02-06 23:32:09 -0500

    Slightly modify the prime_sieve tests

--- tests/test_prime_sieve.py
+++ tests/test_prime_sieve.py
@@ -7,8 +7,14 @@ from prime_lib import prime_sieve
 
 class TestPrimeSieve(unittest.TestCase):
 
-    def test_prime_sieve_for_small_limits(self):
-        self.assertListEqual(prime_sieve(4), [False, False, True, True])
+    def test_prime_sieve_at_very_small_values(self):
+        self.assertListEqual(prime_sieve(0), [])
+        self.assertListEqual(prime_sieve(1), [False])
+        self.assertListEqual(prime_sieve(2), [False, False])
+        self.assertListEqual(prime_sieve(3), [False, False, True])
+
+    def test_prime_sieve_for_small_numbers(self):
+        self.assertListEqual(prime_sieve(5), [False, False, True, True, False])
         self.assertListEqual(
             prime_sieve(10),
             [
@@ -26,12 +32,9 @@ class TestPrimeSieve(unittest.TestCase):
             ],
         )
 
-    def test_prime_sieve_for_zero(self):
-        self.assertListEqual(prime_sieve(0), [])
-
-    def test_prime_sieve_using_indices(self):
+    def test_prime_sieve_using_prime_indices(self):
         sieve = prime_sieve(10)
         self.assertListEqual([n for n in range(10) if sieve[n]], [2, 3, 5, 7])
 
-    def test_prime_sieve_sum_one_hundred(self):
+    def test_prime_sieve_sum_to_one_hundred(self):
         self.assertEqual(sum(prime_sieve(100)), 25)
